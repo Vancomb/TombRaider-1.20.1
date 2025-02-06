733c89988a04cf9b393207c7118bd264e8bea661
Vancomb <vancomb97@gmail.com>
2025-02-06 16:57:02 -0500

    Initial Commit

--- src/main/java/net/vancomb/tombraider/item/custom/SurvivorPickaxeItem.java
+++ src/main/java/net/vancomb/tombraider/item/custom/SurvivorPickaxeItem.java
@@ -1,11 +1,13 @@
 package net.vancomb.tombraider.item.custom;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
-import net.vancomb.tombraider.item.ModItems;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -14,16 +16,25 @@ public class SurvivorPickaxeItem extends PickaxeItem {
     public SurvivorPickaxeItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
 
-
-       // ItemStack defaultStack = new ItemStack(this);
-        // defaultStack.enchant(Enchantments.SHARPNESS,2);
     }
 
     @Override
-    public void onCraftedBy(ItemStack stack, Level level, Player player) {
-        // This method should not be necessary if the item isn't craftable, but keeping it in case you decide to make it craftable later.
-        // If you decide to keep it non-craftable, remove this method.
-        stack.enchant(Enchantments.SHARPNESS, 1);
+    public ItemStack getDefaultInstance() {
+        ItemStack stack = new ItemStack(this);
+        stack.enchant(Enchantments.BLOCK_EFFICIENCY,3);
+        return stack;
+    }
+
+
+}
+
+/*
+
+    //ITEM TOOLTIP
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.tombraider.survivor_pickaxe.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
 
     }
 
@@ -33,14 +44,21 @@ public class SurvivorPickaxeItem extends PickaxeItem {
         return false;
     }
 
-    //TOOLTIP
+     @Override
+    public void onCraftedBy(ItemStack stack, Level level, Player player) {
+        // This method should not be necessary if the item isn't craftable, but keeping it in case you decide to make it craftable later.
+        // If you decide to keep it non-craftable, remove this method.
+        stack.enchant(Enchantments.SHARPNESS, 1);
+
+
+Not working
     @Override
-    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
-        pTooltipComponents.add(Component.translatable("tooltip.tombraider.survivor_pickaxe.tooltip"));
-        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
-    }
+    public ItemStack getDefaultInstance() {
+        ItemStack stack = new ItemStack(this);
+        stack.enchant(Enchantments.BLOCK_EFFICIENCY,3);
+        stack.enchant(Enchantments.SHARPNESS,3);
+        stack.enchant(Enchantments.MENDING,1);
+        return stack;
 
-}
-
-
+ */
 
